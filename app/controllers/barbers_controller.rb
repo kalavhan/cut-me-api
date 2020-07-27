763dc79462d0c49d8bb7351dd97b8a75fc65a6ae
kalavhan <josue.brigido1@gmail.com>
2020-07-27 11:33:23 -0600

    Add controller methods for barbers

--- app/controllers/barbers_controller.rb
+++ app/controllers/barbers_controller.rb
@@ -1,7 +1,11 @@
 class BarbersController < ApplicationController
   def index
+    @barber = Barber.all
+    json_response(@barber)
   end
 
   def show
+    @barber = Barber.where("id = ?", params[:id])
+    json_response(@barber)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   post '/users/signin', to: 'users#signin'
   post '/users/signup', to: 'users#signup'
   get '/barbers/index', to: 'barbers#index'
-  get '/barbers/show', to: 'barbers#show'
+  get '/barbers/show/:id', to: 'barbers#show'
 end
