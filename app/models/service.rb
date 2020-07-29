cb008c8ab1420f435c3ab3824257e5300855d4cb
kalavhan <josue.brigido1@gmail.com>
2020-07-29 00:33:07 -0600

    Add price validation for services

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,5 @@ class Service < ApplicationRecord
   belongs_to :barber
   validates :barber_id, presence: true
   validates :title, presence: true, length: { maximum: 80 }
+  validates :price, presence: true
 end
