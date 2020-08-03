85aae358557a2a56b01545975e5e085c4bb0cfad
kalavhan <josue.brigido1@gmail.com>
2020-08-03 16:02:55 -0600

    Add more tests to appointments model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  it { should belong_to(:user) }
-  it { should belong_to(:barber) }
   it { should validate_presence_of(:appt_date) }
+  it { should allow_value('July 28, 2018 18:36:00').for(:appt_date) }
+  it { should_not allow_value('three days from today').for(:appt_date) }
+
   it { should validate_presence_of(:barber_id) }
+
   it { should validate_presence_of(:user_id) }
+
+  it { should belong_to(:user) }
+  
+  it { should belong_to(:barber) }
 end
