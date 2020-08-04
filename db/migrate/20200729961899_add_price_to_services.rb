83f31391b1a327ef24019f42cd1a97f3347425b6
kalavhan <josue.brigido1@gmail.com>
2020-08-04 10:45:24 -0600

    Add using property to migration

--- db/migrate/20200729961899_add_price_to_services.rb
+++ db/migrate/20200729961899_add_price_to_services.rb
@@ -1,5 +1,5 @@
 class AddPriceToServices < ActiveRecord::Migration[6.0]
   def change
-    change_column :services, :price, :decimal, precision: 5, scale: 2
+    change_column :services, :price, :decimal, precision: 5, scale: 2, using: 'price::decimal(5,2)'
   end
 end
