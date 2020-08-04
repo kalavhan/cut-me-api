fa8cd3cfa244bb8e04df18a06291c0b103fcf012
kalavhan <josue.brigido1@gmail.com>
2020-08-04 10:24:30 -0600

    Remove unnecesary migrations

--- db/migrate/20200727154617_create_services.rb
+++ db/migrate/20200727154617_create_services.rb
@@ -3,7 +3,7 @@ class CreateServices < ActiveRecord::Migration[6.0]
     create_table :services do |t|
       t.string :title
       t.integer :barber_id
-
+      t.money :price
       t.timestamps
     end
   end
--- db/migrate/20200729961899_add_price_to_services.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToServices < ActiveRecord::Migration[6.0]
-  def change
-    add_column :services, :price, :money
-  end
-end
